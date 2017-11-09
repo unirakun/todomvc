18ca9e0be9dd59cfff91dbcd44a8ed08ae26bee8
guigail <guillaume.crespel@gmail.com>
2017-11-09 11:24:40 +0100

    review

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className="info">
     <p>Double-click to edit a todo</p>
-    <p>Created by <a href="https://github.com/Alakarteio"><b>Alakarte - 🐲</b></a></p>
+    <p>Created by <a href="https://github.com/alakarteio"><b>Alakarte - 🐲</b></a></p>
     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
   </div>
 )
