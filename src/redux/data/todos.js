97bef1678e7ea5337c3ed545fd4f48a7ea4f7ed0
guigail <guillaume.crespel@gmail.com>
2017-11-22 15:29:31 +0100

    :arrow_up: k-redux-factory

--- src/redux/data/todos.js
+++ src/redux/data/todos.js
@@ -3,4 +3,4 @@ import { mapPayload } from 'k-redux-factory/helpers'
 
 /* Todos Reducer  with her mapper */
 export const todosMapper = todo => ({ id: new Date().getTime(), todo: todo.trim() })
-export default keyValue({ pre: [mapPayload(/@trampss\/ADD_TODOS/)(todosMapper)] })({ path: 'data', key: 'id', name: 'todos' })
+export default keyValue({ pre: [mapPayload(/@@krf\/ADD_TODOS/)(todosMapper)] })({ path: 'data', key: 'id', name: 'todos' })
